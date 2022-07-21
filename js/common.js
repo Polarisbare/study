2b84326625330b607c8b26cbb1f336da2d10377f
lvjingxin <1768538073@qq.com>
2022-07-21 10:38:45 +0800

    传入数据铺设

--- js/common.js
+++ js/common.js
@@ -26,8 +26,8 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
   // 2xx 范围内的状态码都会触发该函数。
   // 对响应数据做点什么
-  console.log(response)//response里面就是返回给逻辑页面的结果对象axios封装结果
-  console.log(response.data)
+  // console.log(response)//response里面就是返回给逻辑页面的结果对象axios封装结果
+  // console.log(response.data)
   // 例如我不想让页面那边在点两个data
   // return会把结果返回到use方法（axios源码内），然后axios掩码会交给逻辑页面（axios请求的地方）为Promise的成功结果
   // 我们在这里去除后台返回的真正数据对象，交给逻辑页面
